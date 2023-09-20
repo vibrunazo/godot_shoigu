03dcc02272e5139f0fe8bb8e2b1a779bd4f0726c
Vandré Brunazo <vibrunazo@gmail.com>
2023-09-20 16:54:17 -0300

    faster intro

--- scenes/opening.gd
+++ scenes/opening.gd
@@ -1,7 +1,6 @@
 extends CanvasLayer
 
 var state: int = 0
-var music_tween: Tween
 
 var music_volume_min: = -64
 var music_volume_max: = -12
@@ -10,7 +9,7 @@ var music_volume: = -12
 func _ready():
 	$VideoTalkLoop.finished.connect(on_talk_loop_finished)
 	$AudioMusicLoop.volume_db = -60
-	music_fade_in()
+	music_fade_in($AudioMusicLoop)
 	$BirdIntro.visible = false
 
 #func _process(delta):
@@ -25,28 +24,31 @@ func play():
 	state = 1
 	$AnimClick.play("clicked")
 	$AudioMusicLoop.get_stream().loop = false
-	await $AudioMusicLoop.finished
+#	await $AudioMusicLoop.finished
+	await get_tree().create_timer(1).timeout
+	music_fade_out($AudioMusicLoop)
+	music_fade_in($AudioMusicIntro, 5)
 	$AudioMusicIntro.play()
 #	await  $AudioMusicIntro.finished
-	await get_tree().create_timer(13).timeout
+	await get_tree().create_timer(5).timeout
 	$Anim.play("fade_out")
 	$VideoStreamPlayer.play()
-	await get_tree().create_timer(4).timeout
+	await get_tree().create_timer(4.1).timeout
 	$AnimShoigu.play("start")
 	$AudioMusicMain.play()
 	await get_tree().create_timer(5).timeout
-	music_fade_out()
-	await get_tree().create_timer(2).timeout
+	music_fade_out($AudioMusicMain, 6)
+	await get_tree().create_timer(2.5).timeout
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 		
 func on_talk_loop_finished():
 	$VideoTalkLoop.play()
 
 	
-func music_fade_in():
-	music_tween = create_tween()
-	music_tween.tween_property($AudioMusicLoop, "volume_db", music_volume, 2)
+func music_fade_in(widget, duration = 2):
+	var tween = create_tween()
+	tween.tween_property(widget, "volume_db", music_volume, duration)
 
-func music_fade_out():
-	music_tween = create_tween()
-	music_tween.tween_property($AudioMusicMain, "volume_db", music_volume_min, 2)
+func music_fade_out(widget, duration = 2):
+	var tween = create_tween()
+	tween.tween_property(widget, "volume_db", -60, duration)
